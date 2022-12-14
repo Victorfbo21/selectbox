6b0b6d90e86c828b00b4a1f896fc32a978177063
Victor Bentes <victorfbo@outlook.com>
2022-12-14 16:24:22 -0300

    selectbox-cidades

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+import useCidades from './hooks/Cidades';
+import useEstados from './hooks/Estados';
 
 function App() {
+  const { estados } = useEstados();
+  const { cidades }= useCidades();
   return (
+    <>
     <div className="App">
-    
+      <select>
+         {estados.map((estado) =>(<option>{estado.nome}</option>))}
+      </select>
+      <select>
+          {cidades.map((cidade)=> (<option>{cidade.nome}</option>))}
+      </select>
+
     </div>
+    </>
   );
 }
 
--- my-app/src/hooks/Estados.tsx
+++ my-app/src/hooks/Estados.tsx
@@ -1,7 +1,35 @@
-import { useState } from "react";
-const useEstados = () => {
-    const  [estados , setEstados] = useState([])
+import { useEffect, useState } from "react";
 
+export interface Regiao {
+    id: number;
+    sigla: string;
+    nome: string;
+}
+
+export interface IEstados {
+    id: number;
+    sigla: string;
+    nome: string;
+    regiao: Regiao;
+}
+
+
+const useEstados = () => {
+    const  [estados , setEstados] = useState<IEstados[]>([])
+
+    useEffect( () => {
+        fetch("https://brasilapi.com.br/api/ibge/uf/v1")
+        .then((response)=> response.json())
+        .then((data) => setEstados(data))
+        }, []);
+    
+        
+            
+     
+        return {
+            estados
+            }
+        
 }
 
 export default useEstados;
